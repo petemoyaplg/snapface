bf9162e04baee3e0a352bff8bd54df0998d3a986
STAGIAIRE IT <stagiaire-it@local.rawsur.com>
2022-04-05 18:38:43 +0100

    add ngIfcondition

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit
+{
+  @Input() faceSnap!: FaceSnap;
+  buttonText!: String;
+
+  ngOnInit(): void {
+    this.buttonText = 'Oh Snap !';
+  }
+
+  onSnap(): void {
+    console.log('====================================');
+    console.log("onSnap");
+    console.log('====================================');
+    if (this.buttonText === 'Oh Snap !') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oups, unSnap !';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'Oh Snap !';
+    }
+  }
+}
